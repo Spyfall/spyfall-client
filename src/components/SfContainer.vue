ece2484a7b0bba63865c79e69aa292d3da9aa379
YusufcanY <playders03@gmail.com>
2021-05-07 02:44:22 +0300

    sfcontainer component

---
+++ src/components/SfContainer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-spyfall-blue w-full h-screen justify-center items-center flex">
+    <div
+      class="flex flex-row flex-wrap w-3/4 md:w-1/2 bg-white rounded-lg shadow-xl"
+    >
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
