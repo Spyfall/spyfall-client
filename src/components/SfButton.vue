b2063d07922363735c19b9bcf3779599c269df20
YusufcanY <playders03@gmail.com>
2021-05-06 17:41:15 +0300

    landing container updated

---
+++ src/components/SfButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <button
+    class="text-white rounded-full py-2 px-6 text-lg focus:outline-none hover:shadow-lg"
+    :class="[handleBg]"
+  >
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  computed: {
+    handleBg() {
+      return `bg-${this.background}`;
+    },
+  },
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    background: {
+      type: String,
+      default: "spyfall-red",
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,13 +1,28 @@
 <template>
-  <div class="bg-red-200">sa bura landing ve spyfall</div>
+  <div class="bg-spyfall-blue w-full h-screen justify-center items-center flex">
+    <div class="flex flex-row w-1/2 bg-white rounded-lg shadow-xl">
+      <div class="flex flex-col flex-1 justify-center items-center p-4">
+        <div class="rounded-full w-20 h-20 bg-spyfall-yellow"></div>
+        <input
+          type="text"
+          placeholder="Username"
+          class="bg-spyfall-yellow rounded-full px-3 py-1 my-4 outline-none placeholder-gray-300 text-white"
+        />
+        <sf-button text="PLAY"></sf-button>
+      </div>
+      <div class="flex flex-col flex-1 p-4 justify-around">
+        <sf-button text="Find Lobby" background="spyfall-blue"></sf-button>
+        <sf-button text="Create" background="spyfall-blue"></sf-button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import SfButton from "../components/SfButton.vue";
 export default {
-
-}
+  components: { SfButton },
+};
 </script>
 
-<style>
-
-</style>
+<style scoped></style>
