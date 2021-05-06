6c35b032431af8cc773f582326256e866c5827ff
YusufcanY <playders03@gmail.com>
2021-05-06 17:38:51 +0300

    tailwind config created

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        "spyfall-blue": "#21235C",
+        "spyfall-yellow": "#FCB131",
+        "spyfall-red": "#E43C3B"
+      }
+    }
+  },
+}
