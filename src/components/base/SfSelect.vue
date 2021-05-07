9fa1a28b895b814862bfbaff85c676d78b599bba
YusufcanY <playders03@gmail.com>
2021-05-08 02:58:44 +0300

    base select component added

---
+++ src/components/base/SfSelect.vue
@@ -0,0 +1,24 @@
+<template>
+  <select
+    class="text-spyfall-yellow p-1 rounded-t-md border-b-4 border-spyfall-yellow hover:bg-gray-200 focus:bg-gray-200 outline-none duration-200"
+  >
+    <slot />
+  </select>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: "text",
+    },
+    placeholder: {
+      type: String,
+      default: "placeholder",
+    },
+  },
+};
+</script>
+
+<style></style>
