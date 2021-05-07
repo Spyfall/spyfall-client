6a03dbe3cb9f3047841fd735cb503ad4d0d1d78a
YusufcanY <playders03@gmail.com>
2021-05-08 02:58:30 +0300

    base input component added

---
+++ src/components/base/SfInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <input
+    :type="type"
+    :placeholder="placeholder"
+    class="text-spyfall-yellow p-1 rounded-t-md border-b-4 border-spyfall-yellow hover:bg-gray-200 focus:bg-gray-200 outline-none duration-200"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: "text",
+    },
+    placeholder: {
+      type: String,
+      default: "placeholder",
+    },
+  },
+};
+</script>
+
+<style></style>
