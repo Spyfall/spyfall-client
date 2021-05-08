d8b158410e0c94071bbbe077960fb806cebdaf24
YusufcanY <playders03@gmail.com>
2021-05-08 04:50:18 +0300

    base container padding added

--- src/components/base/SfContainer.vue
+++ src/components/base/SfContainer.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-spyfall-blue w-full h-screen justify-center items-center flex">
     <div
-      class="flex flex-row flex-wrap w-3/4 md:w-1/2 bg-white rounded-lg shadow-xl"
+      class="flex flex-row flex-wrap w-3/4 md:w-1/2 p-0 md:p-2 xl:p-4 bg-white rounded-lg shadow-xl"
     >
       <slot />
     </div>
